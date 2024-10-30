8ae552ca5822e7fbe9f50241bab6e9e733049f4d
Rounak Paul <rounakpaul@hotmail.com>
2024-10-30 09:45:57 +0530

    added documentation to SystemManager

--- Quasar/src/Core/SystemManager.cpp
+++ Quasar/src/Core/SystemManager.cpp
@@ -7,8 +7,6 @@ b8 SystemManager::init() {
     assert(!instance);
     instance = new SystemManager();
 
-    instance->registered_systems = static_cast<System**>(QSMEM.allocate(SYSTEM_MAX * sizeof(System*)));
-
     for (int i=0; i<SYSTEM_MAX; i++) {
         instance->registered_systems[i] = nullptr;
     }
@@ -16,13 +14,16 @@ b8 SystemManager::init() {
 }
 
 void SystemManager::shutdown() {
-    QSMEM.free(instance->registered_systems);
     delete instance;
 }
 
 b8 SystemManager::Register(qs_system s_id, System* s, void* config) {
     if (s == nullptr) {
         LOG_ERROR("ptr to System [%d] can not be null", s_id)
+        return false;
+    }
+    if (registered_systems[s_id] != nullptr) {
+        LOG_ERROR("System [%d] already exists, can not override existing system", s_id)
         return false;
     }
     registered_systems[s_id] = s;
--- Quasar/src/Core/SystemManager.h
+++ Quasar/src/Core/SystemManager.h
@@ -8,6 +8,10 @@
 
 namespace Quasar
 {
+    /**
+     * @brief Quasar Engine's all systems
+     * 
+     */
     typedef enum qs_system {
         SYSTEM_EVENT,
         SYSTEM_INPUT,
@@ -23,7 +27,21 @@ namespace Quasar
 
         System* get_system(qs_system s_id) {return registered_systems[s_id];}
 
+        /**
+         * @brief Register a system to the system manager, calls init() of the System, this SystemManager becomes owner of the system instance
+         * 
+         * @param s_id system's target id, Note: one system registration per id 
+         * @param s Pointer to the System
+         * @param config initial config data, used with init(config)
+         * @return b8 Registration is successful. If System is nullptr or override registration returns false
+         */
         b8 Register(qs_system s_id, System* s, void* config);
+
+        /**
+         * @brief Unregister the system from the SystemManager, free System memory and ends ownership of the System
+         * 
+         * @param s_id System id to be Unregistered
+         */
         void Unregister(qs_system s_id);
 
         private:
@@ -34,7 +52,7 @@ namespace Quasar
          * @brief array to hold registered systems for the core engine, allocated on init(), destroyed on shutdown()
          * 
          */
-        System** registered_systems;
+        System* registered_systems[SYSTEM_MAX];
     };
 
     #define QS_SYSTEM_MANAGER SystemManager::get_instance()
