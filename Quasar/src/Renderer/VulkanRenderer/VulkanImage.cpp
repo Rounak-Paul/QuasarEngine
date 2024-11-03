f7011f2fa415065eced5d0e0224c95a62b378679
Rounak Paul <rounakpaul@hotmail.com>
2024-11-03 11:10:21 +0530

    Update VulkanImage.cpp

--- Quasar/src/Renderer/VulkanRenderer/VulkanImage.cpp
+++ Quasar/src/Renderer/VulkanRenderer/VulkanImage.cpp
@@ -6,42 +6,53 @@ VkImageSubresourceRange image_subresource_range(VkImageAspectFlags aspect_mask);
 
 void transition_image(VkCommandBuffer cmd, VkImage image, VkImageLayout current_layout, VkImageLayout new_layout)
 {
-    VkImageMemoryBarrier2 image_barrier {.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2};
+    VkImageMemoryBarrier image_barrier{};
+    image_barrier.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
     image_barrier.pNext = nullptr;
 
-    image_barrier.srcStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;
-    image_barrier.srcAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT;
-    image_barrier.dstStageMask = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;
-    image_barrier.dstAccessMask = VK_ACCESS_2_MEMORY_WRITE_BIT | VK_ACCESS_2_MEMORY_READ_BIT;
+    // Set source and destination pipeline stages and access masks
+    image_barrier.srcAccessMask = VK_ACCESS_MEMORY_WRITE_BIT;
+    image_barrier.dstAccessMask = VK_ACCESS_MEMORY_WRITE_BIT | VK_ACCESS_MEMORY_READ_BIT;
 
     image_barrier.oldLayout = current_layout;
     image_barrier.newLayout = new_layout;
 
-    VkImageAspectFlags aspect_mask = (new_layout == VK_IMAGE_LAYOUT_DEPTH_ATTACHMENT_OPTIMAL) ? VK_IMAGE_ASPECT_DEPTH_BIT : VK_IMAGE_ASPECT_COLOR_BIT;
-    image_barrier.subresourceRange = image_subresource_range(aspect_mask);
+    VkImageAspectFlags aspect_mask = (new_layout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) 
+                                     ? VK_IMAGE_ASPECT_DEPTH_BIT 
+                                     : VK_IMAGE_ASPECT_COLOR_BIT;
+
+    // Set the subresource range with the aspect mask
+    image_barrier.subresourceRange.aspectMask = aspect_mask;
+    image_barrier.subresourceRange.baseMipLevel = 0;
+    image_barrier.subresourceRange.levelCount = 1;
+    image_barrier.subresourceRange.baseArrayLayer = 0;
+    image_barrier.subresourceRange.layerCount = 1;
     image_barrier.image = image;
 
-    VkDependencyInfo dep_info {};
-    dep_info.sType = VK_STRUCTURE_TYPE_DEPENDENCY_INFO;
-    dep_info.pNext = nullptr;
+    // Define source and destination pipeline stages
+    VkPipelineStageFlags src_stage = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
+    VkPipelineStageFlags dst_stage = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
 
-    dep_info.imageMemoryBarrierCount = 1;
-    dep_info.pImageMemoryBarriers = &image_barrier;
-
-    vkCmdPipelineBarrier2(cmd, &dep_info);
+    // Execute the barrier
+    vkCmdPipelineBarrier(
+        cmd,
+        src_stage,
+        dst_stage,
+        0,
+        0, nullptr,
+        0, nullptr,
+        1, &image_barrier
+    );
 }
 
 void copy_image_to_image(VkCommandBuffer cmd, VkImage source, VkImage destination, VkExtent2D srcSize, VkExtent2D dstSize)
 {
-	VkImageBlit2 blitRegion{ .sType = VK_STRUCTURE_TYPE_IMAGE_BLIT_2, .pNext = nullptr };
+	VkImageBlit blitRegion{};
+	blitRegion.srcOffsets[0] = {0, 0, 0};
+	blitRegion.srcOffsets[1] = {static_cast<int32_t>(srcSize.width), static_cast<int32_t>(srcSize.height), 1};
 
-	blitRegion.srcOffsets[1].x = srcSize.width;
-	blitRegion.srcOffsets[1].y = srcSize.height;
-	blitRegion.srcOffsets[1].z = 1;
-
-	blitRegion.dstOffsets[1].x = dstSize.width;
-	blitRegion.dstOffsets[1].y = dstSize.height;
-	blitRegion.dstOffsets[1].z = 1;
+	blitRegion.dstOffsets[0] = {0, 0, 0};
+	blitRegion.dstOffsets[1] = {static_cast<int32_t>(dstSize.width), static_cast<int32_t>(dstSize.height), 1};
 
 	blitRegion.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
 	blitRegion.srcSubresource.baseArrayLayer = 0;
@@ -53,16 +64,17 @@ void copy_image_to_image(VkCommandBuffer cmd, VkImage source, VkImage destinatio
 	blitRegion.dstSubresource.layerCount = 1;
 	blitRegion.dstSubresource.mipLevel = 0;
 
-	VkBlitImageInfo2 blitInfo{ .sType = VK_STRUCTURE_TYPE_BLIT_IMAGE_INFO_2, .pNext = nullptr };
-	blitInfo.dstImage = destination;
-	blitInfo.dstImageLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
-	blitInfo.srcImage = source;
-	blitInfo.srcImageLayout = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
-	blitInfo.filter = VK_FILTER_LINEAR;
-	blitInfo.regionCount = 1;
-	blitInfo.pRegions = &blitRegion;
+	vkCmdBlitImage(
+		cmd,
+		source,
+		VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+		destination,
+		VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+		1,
+		&blitRegion,
+		VK_FILTER_LINEAR
+	);
 
-	vkCmdBlitImage2(cmd, &blitInfo);
 }
 
 } // namespace Quasar::Vulkan
